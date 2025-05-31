77e5712e3953ee9128476afc63306245eab737a0
César Uriarte <cesar.uriarte@unmsm.edu.pe>
2025-05-31 17:14:04 -0500

    feat: implementar formulario de inicio de sesión con selección de tipo de persona y documento

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
+import { MatOption } from '@angular/material/core';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormField, MatLabel,
+    MatSelect, MatOption,
+    MatInput,
+    MatButton,
+    MatIcon,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export default class LoginComponent {
+  personTypes = signal([{
+    value: 'N',
+    label: 'Natural'
+  }, {
+    value: 'J',
+    label: 'Juridica'
+  }])
 
+  documentTypes = signal([{
+    value: 'DNI',
+    label: 'Documento Nacional de Identidad'
+  }, {
+    value: 'CE',
+    label: 'Carnet de Extranjería'
+  }, {
+    value: 'PASS',
+    label: 'Pasaporte'
+  }])
 }
