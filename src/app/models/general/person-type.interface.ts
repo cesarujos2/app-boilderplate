f88d755cda2e8acfd4c41a801202d726ea577733
César Uriarte <cesar.uriarte@unmsm.edu.pe>
2025-05-31 20:28:11 -0500

    feat: agregar interfaces ValueLabel, DocumentType y PersonType; implementar validación de RUC en el formulario de inicio de sesión

---
+++ src/app/models/general/person-type.interface.ts
@@ -0,0 +1,5 @@
+import { ValueLabel } from "../common/value-label.interface";
+
+export interface PersonType extends ValueLabel {
+    requiredRuc: boolean;
+}
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,33 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSelect } from '@angular/material/select';
 import { MatOption } from '@angular/material/core';
 import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { isRucValidator } from '@validators/sync/ruc/is-ruc.validator';
+import { CommonModule } from '@angular/common';
+import { DocumentType } from '@models/general/document-type.interface';
+import { PersonType } from '@models/general/person-type.interface';
+import { Router } from '@angular/router';
+import { AUTH_PATHS } from '../auth.routes';
 
 @Component({
   selector: 'app-login',
   imports: [
-    MatFormField, MatLabel,
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormField, MatLabel, MatError,
     MatSelect, MatOption,
     MatInput,
     MatButton,
     MatIcon,
+    MatCheckbox
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export default class LoginComponent {
-  personTypes = signal([{
+export default class LoginComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+
+  personTypes = signal<PersonType[]>([{
     value: 'N',
-    label: 'Natural'
+    label: 'Natural',
+    requiredRuc: false
   }, {
     value: 'J',
-    label: 'Juridica'
+    label: 'Juridica',
+    requiredRuc: true
   }])
 
-  documentTypes = signal([{
+  documentTypes = signal<DocumentType[]>([{
     value: 'DNI',
     label: 'Documento Nacional de Identidad'
   }, {
@@ -37,4 +53,54 @@ export default class LoginComponent {
     value: 'PASS',
     label: 'Pasaporte'
   }])
+
+  form: FormGroup = this.fb.group({
+    ruc: [null, [Validators.required, isRucValidator()]],
+    documentType: [null, Validators.required],
+    documentNumber: [null, [Validators.required]],
+    rememberMe: [false],
+    password: [null, [Validators.required, Validators.minLength(6)]],
+  });
+
+  ngOnInit() {
+    this.getLoginData();
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.saveLoginData();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  toRegisterPage(){
+    this.router.navigate(AUTH_PATHS.FULL.REGISTER());
+  }
+
+  private saveLoginData() {
+    if (this.form.value.rememberMe) {
+      localStorage.setItem('ruc', this.form.value.ruc);
+      localStorage.setItem('documentType', this.form.value.documentType);
+      localStorage.setItem('documentNumber', this.form.value.documentNumber);
+      localStorage.setItem('rememberMe', this.form.value.rememberMe.toString());
+    } else {
+      localStorage.removeItem('ruc');
+      localStorage.removeItem('documentType');
+      localStorage.removeItem('documentNumber');
+      localStorage.removeItem('rememberMe');
+    }
+  }
+
+  private getLoginData() {
+    const storedRuc = localStorage.getItem('ruc');
+    const storedDocumentType = localStorage.getItem('documentType');
+    const storedDocumentNumber = localStorage.getItem('documentNumber');
+    const storedRememberMe = localStorage.getItem('rememberMe');
+
+    this.form.patchValue({ ruc: storedRuc });
+    this.form.patchValue({ documentType: storedDocumentType });
+    this.form.patchValue({ documentNumber: storedDocumentNumber });
+    this.form.patchValue({ rememberMe: storedRememberMe === 'true' });
+  }
 }
